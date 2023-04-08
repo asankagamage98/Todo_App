c1d66fb6f2c94bcd8fc62380158245b75bf3275f
Asanka Gamage <asankagamage1998@gmail.com>
2023-04-08 10:44:06 +0530

    create add item part

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View,} from 'react-native';
+import React,{useState} from 'react';
+import Header from './Components/Header';
+import TodoItem from './Components/TodoItem';
+import AddTodo from './Components/AddTodo';
 
 export default function App() {
-  return (
+
+ const [todos,setTodos] =useState([
+    {text:'buy coffee',key:'1'},
+    {text:'Create an app',key:'2'},
+    {text:'Play on the switch',key:'3'},
+ ]);
+
+ //same id tyena eka remove krla ithuru tika display krnwa
+ const pressHandler = (key) => {
+  setTodos((prevTodos) => {
+    return prevTodos.filter(todo => todo.key != key);
+  });
+ }
+
+ //add item
+ const submitHandler =(text) => {
+    setTodos((prevTodos)=>{
+      return [
+        { text: text, key: Math.random().toString()},
+        ...prevTodos
+      ]
+    })
+ }
+
+
+return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <Header/>
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) =>(
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+
+          
+
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +56,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  content: {
+    padding:40,
+  },
+  list: {
+    marginTop:20,
+  }
+
+  
+
 });
---
+++ Components/Header.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+export default function Header() {
+  return (
+    <View style={styles.header}>  
+      <Text style={styles.texts}>My Todos</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        height:80,
+        paddingTop:38,
+        backgroundColor:'coral',
+        
+    },
+
+    texts: {
+      fontSize:20,
+      textAlign:'center',
+      color:'#fff',
+      
+    }
+});
